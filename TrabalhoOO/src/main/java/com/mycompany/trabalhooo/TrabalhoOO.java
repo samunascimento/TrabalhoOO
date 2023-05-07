bb5f4e4d300cb0b85bf7eee0161259debac7c79e
AllanChang <allan.jf@yahoo.com>
2023-05-07 14:52:26 -0300

    Commit 01, começando interfaces gráficas, login e registro

---
+++ TrabalhoOO/src/main/java/com/mycompany/trabalhooo/TrabalhoOO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhooo;
+
+/**
+ *
+ * @author Gerenciador
+ */
+public class TrabalhoOO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
